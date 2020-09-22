b7c7402d32bd4d5dc4406976753a78d85e35651c
Alex S Hong <alexsungmin.hong@gmail.com>
2020-09-22 13:51:21 -0400

    Update efficient_shipping.py
    bug fixed

--- roblox/efficient_shipping.py
+++ roblox/efficient_shipping.py
@@ -6,18 +6,18 @@ import heapq
 
 def getMaxUnits(boxes: int, unitsPerBox:list, truckSize:int):
     heap = []
-    for box, unit in zip (boxes, unitsPerBox):
-        heap.append((box-unit, unit, box))
+    for box, price in zip (boxes, unitsPerBox):
+        heap.append((-price, box))
     
-    #min-heap...high value first
+    #min-heap...high unitPrice first as sign reverted
     heapq.heapify(heap)
 
     res = 0
     while heap and truckSize > 0:
-        head = heapq.heappop(heap)
-        res *= head[1] * (head[2] if truckSize > head[2] else truckSize)
-        truckSize -= head[2]
-    print(res)
+        price, box = heapq.heappop(heap)
+        price = -price
+        res += price * (box if truckSize > box else truckSize)
+        truckSize -= box
     return res
 
 
@@ -25,5 +25,6 @@ def getMaxUnits(boxes: int, unitsPerBox:list, truckSize:int):
 
 if __name__ == "__main__":
     assert getMaxUnits([1,2,3], [3,2,1],3) == 7
-    print ("------")
     assert getMaxUnits([3,1,6], [2,7,4], 6) == 27
+    assert getMaxUnits([1],[2], 1) == 2
+
